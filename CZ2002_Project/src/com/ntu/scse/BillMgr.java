96194a093d8d5072c9e5f2b655e6f856b1e6f94a
lzcaisg <lz-cai@foxmail.com>
2018-04-19 15:45:57 +0800

    Update the diagram in order to fetch

--- CZ2002_Project/src/com/ntu/scse/BillMgr.java
+++ CZ2002_Project/src/com/ntu/scse/BillMgr.java
@@ -2,9 +2,7 @@ package com.ntu.scse;
 
 import java.util.ArrayList;
 public class BillMgr {
-//    Functional Requirement:
-//    1. Given a Guest ID, read/write his/her current bills from file
-//    2. If new Guest(While Check In), create a new file for him (And add the room bill)
+
 	
 	static ArrayList<Bill> billList = null;
 	private String[] bStatus = { "OPEN", "PAID" };
