5caab792a645d25453db84e4655feebdbe1c8f8b
Christopher Lim <37795828+Limzui@users.noreply.github.com>
2018-04-19 21:50:48 +0800

    Finished order javadoc

--- CZ2002_Project/src/com/ntu/scse/Order.java
+++ CZ2002_Project/src/com/ntu/scse/Order.java
@@ -17,14 +17,54 @@ import java.time.LocalTime;
  */
 public class Order implements Serializable, Comparable<Order> {
 	/**
-	 A variable to store
+	 A variable to store dates
 	 */
 	private LocalDateTime dt;
+	/**
+	 An array to store the possible Order statuses
+	 */
 	private String[] arrayStatus = { "Confirmed", "Preparing", "Delivered" };
-	private String roomNo, ordFName, ordRemarks, dateTime, status;
+	/**
+	 The room number associated with this order
+	 */
+	private String roomNo;
+	/**
+	 The name of the food in this order
+	 */
+	private String ordFName;
+	/**
+	 The remarks (if any) of the food in this order
+	 */
+	private String ordRemarks;
+	/**
+	 The date and time the order was placed
+	 */
+	private String dateTime;
+	/**
+	 The status of this order
+	 */
+	private String status;
+	/**
+	 The price of the food in this order
+	 */
 	private float price;
-	private int quan, orderID;
-
+	/**
+	 The quantity of the food in this order
+	 */
+	private int quan;
+	/**
+	 The ID number of this order
+	 */
+	private int orderID;
+	/**
+	 * Creates a new Order with the given food details.
+	 * @param rNo The room number associated with this order
+	 * @param oID The order identification number
+	 * @param fn The food name in the order
+	 * @param c The food's price in the order
+	 * @param q The quantity of the food in the order
+	 * @param r The food's remarks (if any) in the order
+	 */
 	public Order(String rNo, int oID, String fn, float c, int q, String r) {
 		roomNo = rNo;
 		orderID = oID;
@@ -34,78 +74,88 @@ public class Order implements Serializable, Comparable<Order> {
 		ordRemarks = r;
 		status = arrayStatus[0];
 	}
-
+	/**
+	 * Gets the food name in the order.
+	 * @return the food name in the order.
+	 */
 	public String getOrdFName() {
 		return ordFName;
 	}
-
+	/**
+	 * Gets the food remarks (if any) in the order.
+	 * @return the food remarks (if any) in the order.
+	 */
 	public String getOrdRemarks() {
 		return ordRemarks;
 	}
-
+	/**
+	 * Gets the price of the food.
+	 * @return the price of the food.
+	 */
 	public float getPrice() {
 		return price;
 	}
-
-	public String getStatus() {
-		return arrayStatus[checkTiming()];
-	}
-
+	/**
+	 * Gets the current status of the order.
+	 * @return the current status of the order.
+	 */
+	public String getStatus() { return arrayStatus[checkTiming()]; }
+	/**
+	 * Gets the quantity of the food item in this order.
+	 * @return the quantity of the food item in this order.
+	 */
 	public int getQuan() {
 		return quan;
 	}
-
+	/**
+	 * Gets the room number associated with this order.
+	 * @return the room number associated with this order.
+	 */
 	public String getRoomNo() {
 		return roomNo;
 	}
-
+	/**
+	 * Gets the unique identifier of this order.
+	 * @return the unique identifier of this order.
+	 */
 	public int getID() {
 		return orderID;
 	}
+	/**
+	 * Gets the date and time the order was placed.
+	 * @return the date and time the order was placed.
+	 */
+	public String getDateTime() { return dateTime; }
 
+	/**
+	 * Sets the status of this order.
+	 * @param choice the choice of status (from the array) to set.
+	 */
 	public void setStatus(int choice) {
 		this.status = arrayStatus[choice];
 	}
-
+	/**
+	 * Sets the remarks of this order.
+	 * @param ordRemarks the new remarks to set.
+	 */
 	public void setOrdRemarks(String ordRemarks) {
 		this.ordRemarks = ordRemarks;
 	}
-
+	/**
+	 * Sets the quantity of food in this order.
+	 * @param quan the new quantity of food to set.
+	 */
 	public void setQuan(int quan) {
 		this.quan = quan;
 	}
-
-	public String getDateTime() {
-		return dateTime;
-	}
-
-	public void setOrderDateTime(String time, LocalDateTime lDateTime) {
-
-		dt = lDateTime;
-		dateTime = time;
-		status = arrayStatus[0];
-	}
-
-	public String setOrderDateTime(LocalDateTime lDateTime) {
-
-		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-		String formatDateTime = lDateTime.format(format);
-
-		return formatDateTime;
-	}
-
+	/**
+	 * Sets the quantity of food in this order.
+	 * @return quan the new quantity of food to set.
+	 */
 	private int checkTiming() {
 		LocalDateTime tempDateTime = LocalDateTime.now();
 
-		long minutes = dt.until(tempDateTime, ChronoUnit.MINUTES);
-		minutes = ChronoUnit.MINUTES.between(dt, tempDateTime);
-
-		long seconds = dt.until(dt, ChronoUnit.SECONDS);
-		seconds = ChronoUnit.SECONDS.between(dt, tempDateTime);
-
-		// for checking purpose
-		// System.out.println("current timing: " + tempDateTime);
-		// System.out.println(minutes + " min \t" + seconds + " seconds " );
+		long minutes = ChronoUnit.MINUTES.between(dt, tempDateTime);
 
 		if (minutes > 1)
 			return 2;
@@ -114,7 +164,9 @@ public class Order implements Serializable, Comparable<Order> {
 		else
 			return 0;
 	}
-
+	/**
+	 * Overwritten method for sorting Order Items according to their ID
+	 */
 	@Override
 	public int compareTo(Order comparesTo) {
 		int compareID=(comparesTo).getID();
